c6c01f1f379a2891ef783ace451629377b476e50
Andrea Fàbregas Martínez <a.fabregas@outlook.es>
2022-06-21 17:56:16 +0200

    All main iterations + bonus done.

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((first, second) => {
+      if(first > second) {
+        return 1;
+      } else if(first < second) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items.indexOf(pos) === -1) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    let add = 0;
+    this.items.forEach((number) => {
+      add += number;
+    });
+    return add;
+  }
 
-  avg() {}
+  avg() {
+    let div = 0;
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      this.items.forEach((numb) => {
+        div += numb;
+      });
+    }
+    return div / this.items.length;
+  }
 }
 
 module.exports = SortedList;
